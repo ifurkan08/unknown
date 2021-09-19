a13a6c0f33c051df5dec112a8e7e2c6738f716a8
ifurkan08 <i.furkanwwe@gmail.com>
2021-09-19 22:48:33 +0300

    first commit

---
+++ src/test/java/com/xgroup/unknown/UnknownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xgroup.unknown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnknownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
